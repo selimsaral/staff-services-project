65185281f7f7cc1867686d036ff5ed8902d3ad95
selimsaral <saral_selim@hotmail.com>
2020-12-08 00:03:18 +0300

    Add Models

---
+++ app/Models/Job.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Support\Carbon;
+
+class Job extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'employee_id',
+        'city_id',
+        'county_id',
+        'address',
+        'date',
+        'started_at',
+        'finished_at',
+        'status'
+    ];
+
+    public function employee(): HasOne
+    {
+        return $this->hasOne(Employee::class, 'id', 'employee_id');
+    }
+}
