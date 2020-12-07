2a1efcb383100a51f33ffbadb04861fdc294fd17
selimsaral <saral_selim@hotmail.com>
2020-12-08 00:00:35 +0300

    Add Migration Files

---
+++ database/migrations/2020_12_07_140516_create_counties_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCountiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('counties', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->foreignId('city_id')->index();
+
+            $table->foreign('city_id')->references('id')->on('cities');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('counties');
+    }
+}
---
+++ database/migrations/2020_12_07_140601_create_jobs_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 150);
+            $table->text('description');
+            $table->foreignId('employee_id');
+            $table->foreignId('city_id');
+            $table->foreignId('county_id');
+            $table->text('address');
+            $table->date('date');
+            $table->time('started_at');
+            $table->time('finished_at');
+            $table->enum('status', ['İş Oluşturuldu', 'İşlemde', 'İş Tamamlandı']);
+            $table->integer('priority')->default(0);
+            $table->timestamps();
+
+            $table->foreign('employee_id')->references('id')->on('employees');
+            $table->foreign('city_id')->references('id')->on('cities');
+            $table->foreign('county_id')->references('id')->on('counties');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('works');
+    }
+}
