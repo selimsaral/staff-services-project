0458d456c3d1d230c804d9c90b1664bc21f9861e
selimsaral <saral_selim@hotmail.com>
2020-12-07 16:45:47 +0300

    add auth files

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\HomeController;
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [LoginController::class, 'showLoginForm']);
+
+Auth::routes([
+    'register' => false,
+]);
+
+Route::group(['prefix' => 'admin'], function () {
+
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+
 });
