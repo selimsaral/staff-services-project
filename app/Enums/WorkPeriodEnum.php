47a13548654d936692fcf85ce92c2689d5475a90
selimsaral <saral_selim@hotmail.com>
2020-12-08 00:04:44 +0300

    Add Enums

---
+++ app/Enums/WorkPeriodEnum.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enums;
+
+class WorkPeriodEnum
+{
+    const periods = [
+        1 => ['start' => '09:00:00', 'end' => '12:00:00'],
+        2 => ['start' => '12:00:00', 'end' => '18:30:00'],
+        3 => ['start' => '18:30:00', 'end' => '21:00:00']
+    ];
+}
