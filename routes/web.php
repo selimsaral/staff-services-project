59946dacd3db5257ec133a9b506536fc8fcbd522
selimsaral <saral_selim@hotmail.com>
2020-12-08 00:04:26 +0300

    Update Routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Admin\EmployeeController;
 use App\Http\Controllers\Admin\HomeController;
+use App\Http\Controllers\Admin\WorkController;
 use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
@@ -21,8 +23,23 @@ Auth::routes([
     'register' => false,
 ]);
 
-Route::group(['prefix' => 'admin'], function () {
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function () {
 
     Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+    Route::group(['prefix' => 'employee'], function () {
+        Route::get('/', [EmployeeController::class, 'index'])->name('employee-list');
+        Route::post('/create', [EmployeeController::class, 'create'])->name('employee-create');
+        Route::get('/show/{employee}', [EmployeeController::class, 'show'])->name('employee-show');
+        Route::post('/update/{employee}', [EmployeeController::class, 'update'])->name('employee-update');
+    });
+
+    Route::group(['prefix' => 'job'], function () {
+        Route::get('/', [WorkController::class, 'index'])->name('job-list');;
+        Route::post('/create', [WorkController::class, 'create'])->name('job-create');
+        Route::get('/show/{job}', [WorkController::class, 'show'])->name('job-show');
+        Route::post('/update/{job}', [WorkController::class, 'update'])->name('job-update');
+        Route::post('/get-counties', [WorkController::class, 'getCounties'])->name('get-counties');
+    });
 
 });
