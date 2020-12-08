e08c68dee208d0b197322b54f0f74361ad94e6af
selimsaral <saral_selim@hotmail.com>
2020-12-08 18:55:47 +0300

    Update Job Status

--- app/Enums/JobStatusEnum.php
+++ app/Enums/JobStatusEnum.php
@@ -10,4 +10,9 @@ class JobStatusEnum
         3 => "İşlemde",
         4 => "İş Tamamlandı",
     ];
+
+    const INIT = 1;
+    const GOING = 2;
+    const IN_PROCESS = 3;
+    const COMPLETED = 4;
 }
