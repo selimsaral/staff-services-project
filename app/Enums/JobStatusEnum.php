34499e95b39f5c5e7a0f2fc7fbeabbc6bc6bef61
selimsaral <saral_selim@hotmail.com>
2020-12-08 16:27:17 +0300

    Add Job Status Enums File

---
+++ app/Enums/JobStatusEnum.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Enums;
+
+class JobStatusEnum
+{
+    const STATUSES = [
+        1 => "İş Oluşturuldu",
+        2 => "İş'e Gidiliyor",
+        3 => "İşlemde",
+        4 => "İş Tamamlandı",
+    ];
+}
